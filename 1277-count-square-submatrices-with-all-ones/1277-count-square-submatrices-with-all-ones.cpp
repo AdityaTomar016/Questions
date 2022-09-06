cda2bf88ce4671d897ecf2a13931b72b587fc17d
AdityaTomar016 <97149793+AdityaTomar016@users.noreply.github.com>
2022-09-07 01:24:01 +0530

    Time: 104 ms (57.87%), Space: 23.5 MB (92.45%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -10,18 +10,13 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 
-                if(matrix[i][j]==0){
-                    continue;
-                }
-                
-                if(i==0 || j==0){
-                    total += matrix[i][j];
-                    continue;
-                }
-                
-  int sum=min(matrix[i-1][j],min(matrix[i-1][j-1],matrix[i][j-1]));
-    
-            matrix[i][j] += sum;
+         if(matrix[i][j] > 0 && i > 0 && j > 0) {
+          
+             int mn = min(matrix[i-1][j], min(matrix[i][j-1], matrix[i-1][j-1]));
+             
+             
+          matrix[i][j] = mn + 1;
+        }
                 
                 total += matrix[i][j];
             }
