07e55dcb7d7288219e55c16a6ca610c7c906058d
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2022-10-25 22:14:12 +0530

    Time: 3 ms (51.97%), Space: 6.3 MB (50.76%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        int n=s.size();
+        stack<char>st;
+        
+        for(int i=0;i<n;i++){
+            
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }
+            else if(s[i] == ')' && !st.empty() && st.top() == '('){
+                st.pop();
+            }
+            else if(s[i] == '}' && !st.empty() && st.top() == '{'){
+                st.pop();
+            }
+            else if(s[i] == ']' && !st.empty() && st.top() == '['){
+                st.pop();
+            }
+            else  st.push(s[i]);
+        }
+        
+        if(st.size()) return false;
+        
+        
+        return true;
+    }
+};
