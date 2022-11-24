e1dab8c4afd90410150e7f619f0938806f90d4f2
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2022-11-25 00:28:05 +0530

    Time: 16 ms (85.14%), Space: 5.9 MB (32.60%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        if(x < 0) return 0;
+        if(x < 0) return false;
         
-        string s = to_string(x);
-        
-        int i=0,j=s.size()-1;
-        
-        while(i<=j){
-            if(s[i]!=s[j]){
-                return false;
-            }
-            
-            i++;j--;
+        long long original = x;
+        long long rev = 0;
+        while(x>0){
+            rev = x%10 + rev*10;
+            x= x/10;
         }
-        
-        return true;
+        return rev==original ? true : false;
     }
 };
