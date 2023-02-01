99557f400d815369fca9918bf96119573853d7b1
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-02-01 11:21:13 +0530

    Time: 79 ms (5.18%), Space: 119.5 MB (5.18%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool solve(string s,int len,string divisor){
+        
+        int i=0;
+        
+        while(i < s.size()){
+            string dividend = s.substr(i,len);
+            
+            if(divisor != dividend) return false;
+            
+            i += len;
+        }
+        
+        return true;
+    }
+    string gcdOfStrings(string str1, string str2) {
+        
+        int n = str1.size();
+        int m = str2.size();
+        
+        if(m > n){
+            gcdOfStrings(str2,str1);
+        }
+        
+        string ans = "";
+        
+        for(int i=0;i<m;i++){
+            
+            string divisor = str2.substr(0,i+1);
+            int len = divisor.size();
+            
+            if(solve(str1,len,divisor) && solve(str2,len,divisor)){
+                ans = divisor;
+            }
+        }
+        return ans;
+    }
+};
