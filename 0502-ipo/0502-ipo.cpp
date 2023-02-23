d1c7ab6b844a8ac50e4152b1ba23d655078b4b1d
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-02-23 13:01:09 +0530

    Time: 230 ms (71.87%), Space: 82 MB (53.52%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -3,28 +3,31 @@ public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         
         int n = capital.size();
-        
         vector<pair<int,int>>v;
         
         for(int i=0;i<n;i++){
             v.push_back({capital[i],profits[i]});
         }
         
-        sort(v.begin(),v.end());
         priority_queue<int,vector<int>>pq;
+        sort(v.begin(),v.end());
+        
+        int i = 0;
+        
+        while(k > 0){
             
-        int idx=0;
-        while(k--){
-            while(idx < n && v[idx].first <= w){
-                pq.push(v[idx].second);
-                idx++;
+            while(i < n && v[i].first <= w){
+                pq.push(v[i].second);
+                i++;
             }
             
             if(pq.empty()) break;
             
             w += pq.top();
             pq.pop();
+            k--;
         }
+        
         return w;
     }
 };
