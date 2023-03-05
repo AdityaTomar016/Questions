8145418f9f8329ad3ab82870e80d369a4d040f98
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-03-05 15:33:30 +0530

    Time: 388 ms (25.33%), Space: 17.2 MB (32.90%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-int solve(int i,int n,vector<int>&nums,vector<int>&dp){
-    if(i == n-1){
-        return 0;
-    }
-
-    if(dp[i] != 1e9){
+    int solve(int i,int n,vector<int>&nums,vector<int>&dp){
+        if(i == n-1){
+            return 0;
+        }
+        
+        if(dp[i] != 1e9){
+            return dp[i];
+        }
+        
+        int curr = nums[i];
+        int ans = 1e9;
+        
+        for(int idx = 1;idx <= curr; idx++){
+            if(i+idx < n)
+            dp[i] = min(dp[i],1+solve(i+idx,n,nums,dp));
+        }
+        
         return dp[i];
     }
-
-    int curr = nums[i];
-    int mini = 1e9;
-
-    for(int it=1;it<=curr;it++){
-        if(i+it < n){
-            mini = min(mini, 1 + solve(i+it,n,nums,dp));
-        }
-    }
-
-    return dp[i] = mini;
-}
+    
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,1e9);
+        vector<int>dp(n,1e9);
+        
         dp[n-1] = 0;
-
+        
         for(int i=n-2;i>=0;i--){
             int curr = nums[i];
-            int mini = 1e9;
+            int ans = 1e9;
 
-            for(int it=1;it<=curr;it++){
-                if(i+it < n){
-                    mini = min(mini, 1 + dp[i+it]);
-                }
+            for(int j = 1;j <= curr; j++){
+                if(i+j < n)
+                dp[i] = min(dp[i],1+dp[i+j]);
             }
-
-            dp[i] = mini;
         }
         return dp[0];
     }
