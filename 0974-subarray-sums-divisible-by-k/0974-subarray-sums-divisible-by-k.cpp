a4dfae5784f2e2726171249c7493c360cccebee5
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-03-13 20:12:23 +0530

    Time: 64 ms (37.08%), Space: 31.7 MB (19.62%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -4,27 +4,26 @@ public:
         
         unordered_map<int,int>mp;
         
-        int curr_sum = 0;
-        int ans=0;
-        
         mp[0] = 1;
+        
+        int currsum = 0;
+        int count = 0;
         
         for(int i=0;i<nums.size();i++){
             
-            curr_sum += nums[i];
-            
-            int rem = curr_sum%k;
+            currsum += nums[i];
+            int rem = currsum%k;
             
             if(rem < 0){
                 rem += k;
             }
             
             if(mp.find(rem) != mp.end()){
-                ans += mp[rem];
+                count += mp[rem];
             }
+            
             mp[rem]++;
         }
-        
-        return ans;
+        return count;
     }
 };
