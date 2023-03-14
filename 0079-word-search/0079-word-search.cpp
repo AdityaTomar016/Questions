266d13bc5df9c58c1bac8d5a53991a0df1c3c959
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-03-14 19:43:19 +0530

    Time: 1097 ms (25.65%), Space: 8.1 MB (62.29%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    bool solve(int i,int j,int k,string word,vector<vector<char>>& board){
+    bool solve(int i,int j,int k,vector<vector<char>>& board, string word){
         
         if(k == word.size()){
             return true;
         }
         
-        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || word[k]!=board[i][j]){
+        if(i >= board.size() || i < 0 || j >= board[0].size() || j<0){
             return false;
         }
         
-        char c = board[i][j];
-        board[i][j] = '.';
-        
-        if (solve(i+1,j,k+1,word,board) ||                              solve(i-1,j,k+1,word,board) ||                              solve(i,j+1,k+1,word,board) ||                              solve(i,j-1,k+1,word,board)){
-            return true;
+        if(board[i][j] != word[k]){
+            return false;
         }
         
-        board[i][j] = c;
+        char ch = board[i][j];
+        board[i][j] = '#';
+        bool flag = false;
         
-        return false;
+        flag = solve(i+1,j,k+1,board,word) ||                              solve(i-1,j,k+1,board,word) ||
+               solve(i,j+1,k+1,board,word) ||
+               solve(i,j-1,k+1,board,word);
+        
+        board[i][j] = ch;
+        
+        return flag;
     }
     bool exist(vector<vector<char>>& board, string word) {
+        int k = 0;
         
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(solve(i,j,0,word,board))
+                if(solve(i,j,0,board,word)){
                     return true;
+                }
             }
         }
-        
         return false;
     }
 };
