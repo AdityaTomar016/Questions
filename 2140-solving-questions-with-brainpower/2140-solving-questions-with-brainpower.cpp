3768f9ce37457caa1de165f4ba1f013659bc43ae
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-05-12 20:29:17 +0530

    Time: 379 ms (55.28%), Space: 115.4 MB (71.24%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -17,7 +17,16 @@ public:
         return dp[i] = max(take,nottake);
     }
     long long mostPoints(vector<vector<int>>& ques) {
-        vector<long long>dp(ques.size(),-1);
-        return solve(0,ques,dp);
+        vector<long long>dp(ques.size()+1,0);
+        
+        for(int i=ques.size()-1;i>=0;i--){
+            long long take = 0, nottake = 0;
+        
+            take += ques[i][0] + solve(i+ques[i][1]+1,ques,dp);
+            nottake = solve(i+1,ques,dp);
+
+            dp[i] = max(take,nottake);
+        }
+        return dp[0];
     }
 };
