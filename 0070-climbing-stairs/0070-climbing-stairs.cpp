1a51a9a7b527e65c75f56a062f6841e7930871cf
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-06-03 15:29:35 +0530

    Time: 4 ms (12.12%), Space: 6.3 MB (16.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -11,7 +11,13 @@ public:
         return dp[i] = solve(i+1,n,dp) + solve(i+2,n,dp);
     }
     int climbStairs(int n) {
-        vector<int>dp(n,-1);
-        return solve(0,n,dp);
+        vector<int>dp(n+2,0);
+        
+        dp[n] = 1;
+        
+        for(int i=n-1;i>=0;i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
     }
 };
