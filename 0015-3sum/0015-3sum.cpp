41bcbaff0da5f8dea69bdd7b69bf2019ae0664f1
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-06-12 02:22:37 +0530

    Time: 218 ms (53.92%), Space: 24.1 MB (73.16%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,49 +5,32 @@ public:
         int n = nums.size();
         sort(nums.begin(),nums.end());
         
-        int st=0;
-        
         vector<vector<int>>ans;
         
         if(nums[0] > 0){
             return {};
         }
-        
-        while(st < n){
+
+        for(int i=0;i<n;i++){
+            int low = i+1,high=n-1;
             
-            if(nums[st] > 0){
-                break;
-            }
-            
-            if(st > 0 && nums[st] == nums[st-1]){
-                st++;
-                continue;
-            }
-            
-            int low = st+1,high=n-1;
-            
-            while(low < high){
-                
-                if(nums[low] + nums[high] == -nums[st]){
-                    ans.push_back({nums[st],nums[low],nums[high]});
+            while(low<high){
+                if(nums[low]+nums[high] == -nums[i]){
+                    ans.push_back({nums[low],nums[high],nums[i]});
                     int lastlow = nums[low];
                     int lasthigh = nums[high];
-
-                    while(low<high && nums[low]==lastlow){
-                        low++;
-                    }
-                    while(low<high && nums[high]==lasthigh){
-                        high--;
-                    }
+                    while(low<high && nums[low] == lastlow) low++;  
+                    while(low<high && nums[high] == lasthigh) high--;
                 }
-                else if(nums[low] + nums[high] < -nums[st]){
+                else if(nums[low]+nums[high] > -nums[i]){
+                    high--;
+                }
+                else {
                     low++;
                 }
-                else high--;
             }
-           
             
-            st++;
+             while(i<n-1 && nums[i] == nums[i+1]) i++;
         }
         
         return ans;
