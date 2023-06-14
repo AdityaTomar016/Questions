ba65300c304a6f21654ff3381fa0415fa31d89e9
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-06-15 01:39:16 +0530

    Time: 4 ms (55.11%), Space: 6 MB (47.90%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        string s="";
-        int num=1;
         
-        for(int i=0;i<n;i++){
-            s += to_string(num);
-            num++;
+        int fact=1;
+        vector<int>nums;
+        
+        for(int i=1;i<n;i++){
+            fact = fact*i;
         }
         
-        for(int i=0;i<k-1;i++){
-            next_permutation(s.begin(),s.end());
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+        }        
+        
+//coz we are considering 0 based indexing and 1234 will be our 0th permutation so bcoz of that the required permutation will be (k-1)th
+        k = k-1;
+        
+        string ans = "";
+        
+        while(true){
+            int currIdx = k/fact;
+            ans += to_string(nums[currIdx]);
+            
+            nums.erase(nums.begin()+currIdx);
+            
+            if(nums.size() == 0){
+                break;
+            }
+            
+            k = k%fact;
+            fact = fact/nums.size();
         }
         
-        return s;
+        return ans;
     }
 };
