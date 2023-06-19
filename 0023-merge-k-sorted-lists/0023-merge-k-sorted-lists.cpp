b391fc4db85ed9d8dde97d92d5ae9093672229e4
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-06-19 19:38:16 +0530

    Time: 33 ms (37.13%), Space: 13.2 MB (83.27%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,23 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class cmp{
+class comp{
   public:
-    bool operator()(ListNode* a,ListNode* b){
-        return a->val > b->val;
-    }
+     bool operator()(ListNode* a, ListNode* b){
+         return a->val > b->val;
+     }
 };
 class Solution {
 public:
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        priority_queue<ListNode*,vector<ListNode*>,cmp>pq;
+        if(lists.size() == 0){
+            return NULL;
+        }
+        
+        priority_queue<ListNode*,vector<ListNode*>,comp>pq;
+        
         
         for(auto i: lists){
-            if(i != NULL){
-                pq.push(i);
-            }
+            if(i != NULL)
+            pq.push(i);
         }
         
         ListNode* dummy = new ListNode(-1);
@@ -35,12 +38,13 @@ public:
             pq.pop();
             curr = curr->next;
             
-            if(curr->next){
+            if(curr->next != NULL){
                 pq.push(curr->next);
             }
             
             curr->next = NULL;
         }
+        
         return dummy->next;
     }
 };
