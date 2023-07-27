73b36a672c1830c78ac4da0d8e7143fe0c7560a9
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-07-27 16:42:15 +0530

    Time: 589 ms (40.59%), Space: 10.5 MB (51.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    int solve(int i,int prev,vector<int>&nums,vector<vector<int>> &dp){
-        if(i >= nums.size()){
-            return 0;
-        }
+//     int solve(int i,int prev,vector<int>&nums,vector<vector<int>> &dp){
+//         if(i >= nums.size()){
+//             return 0;
+//         }
         
-        if(dp[i][prev+1] != -1){
-            return dp[i][prev+1];
-        }
+//         if(dp[i][prev+1] != -1){
+//             return dp[i][prev+1];
+//         }
         
-        int take = 0;
-        int nottake = solve(i+1,prev,nums,dp);
+//         int take = 0;
+//         int nottake = solve(i+1,prev,nums,dp);
         
-        if(prev == -1 || nums[i] > nums[prev]){
-            take += 1 + solve(i+1,i,nums,dp);   
-        }
+//         if(prev == -1 || nums[i] > nums[prev]){
+//             take += 1 + solve(i+1,i,nums,dp);   
+//         }
         
-        return dp[i][prev+1] = max(take,nottake);
-    }
+//         return dp[i][prev+1] = max(take,nottake);
+//     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>dp(n,1);
+        int maxi = 1;
         
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=-1;j--){
-                
-                int take = 0;
-                int nottake = dp[i+1][j+1];
-
-                if(j == -1 || nums[i] > nums[j]){
-                    take = 1 + dp[i+1][i+1];   
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[i] > nums[prev]){
+                    dp[i] = max(dp[i],1 + dp[prev]);
                 }
-
-                dp[i][j+1] = max(take,nottake);
+                
+                maxi = max(maxi,dp[i]);
             }
         }
-        return dp[0][0];
+        
+        return maxi;
     }
 };
