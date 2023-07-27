44f8914c162de7ab6ae311b39710764b6b625d11
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-07-27 15:48:39 +0530

    Time: 1652 ms (11.28%), Space: 292.2 MB (26.61%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-//     int solve(int i,int prev,vector<int>&nums,int n,vector<vector<int>> &dp){
-        
-//         if(i == n){
-//             return 0;
-//         }
-        
-//         if(dp[i][prev+1] != -1) return dp[i][prev+1];
-        
-//         int nottake = solve(i+1,prev,nums,n,dp);
-//         int take=-1;
-        
-//         if(prev == -1 || nums[prev] < nums[i]){
-//             take = 1 + solve(i+1,i,nums,n,dp);
-//         }
-        
-//         return dp[i][prev+1] = max(nottake,take);
-//     }
-    int lengthOfLIS(vector<int>& nums) {  
-        
-        int n = nums.size();
-        
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-
-        
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=-1;j--){
-                
-                int nottake = dp[i+1][j+1];
-                int take = -1;
-                
-                if(j == -1 || nums[j] < nums[i]){
-                    take = 1 + dp[i+1][i+1];
-                }
-                
-                dp[i][j+1] = max(take,nottake);
-            }
+    int solve(int i,int prev,vector<int>&nums,vector<vector<int>> &dp){
+        if(i >= nums.size()){
+            return 0;
         }
         
+        if(dp[i][prev+1] != -1){
+            return dp[i][prev+1];
+        }
         
-        return dp[0][0];
+        int take = 0;
+        int nottake = solve(i+1,prev,nums,dp);
+        
+        if(prev == -1 || nums[i] > nums[prev]){
+            take += 1 + solve(i+1,i,nums,dp);   
+        }
+        
+        return dp[i][prev+1] = max(take,nottake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return solve(0,-1,nums,dp);
     }
 };
