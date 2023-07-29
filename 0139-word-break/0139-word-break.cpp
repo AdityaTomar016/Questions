84a9eada6496384cbd714b5ddb78cae1695b86c0
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-07-29 21:26:52 +0530

    Time: 37 ms (14.24%), Space: 21 MB (9.03%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    bool solve(int i,int prev,string s,unordered_set<string>set,vector<vector<int>> &dp){
+    bool solve(int i,string s,unordered_set<string>set,vector<int> &dp){
         if(i == s.size()){
-            if(prev == s.size()){
-                return true;
-            }
-            else if(set.find(s.substr(prev)) != set.end()){
-                return true;
-            }
+            return true;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        bool take = false;
+        for(int j=i;j<s.size();j++){
+            string word = s.substr(i,j-i+1);
             
-            return false;
+            if(set.find(word) != set.end()){
+                take = take || solve(j+1,s,set,dp);
+            }
         }
         
-        if(dp[i][prev] != -1){
-            return dp[i][prev];
-        }
-        string word = "";
-        word = s.substr(prev, i-prev+1);
-        
-        
-        bool take = false, nottake = false;
-        
-        if(set.find(word) != set.end()){
-            take = take || solve(i+1,i+1,s,set,dp);
-        }
-        nottake = solve(i+1,prev,s,set,dp);
-        
-        return dp[i][prev] = (take || nottake);
+        return dp[i] = take;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string>set;
@@ -35,31 +25,7 @@ public:
         }
         int n = s.size();
         
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        
-        dp[n][n] = 1;
-        
-        for(int i=0;i<n;i++){
-            string word = s.substr(i);
-            if(set.find(word) != set.end()){
-                dp[n][i] = 1;
-            }
-        }
-        
-        for(int i=n-1;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                string word = s.substr(j, i-j+1);
-
-                bool take = false, nottake = false;
-
-                if(set.find(word) != set.end()){
-                    take = take || dp[i+1][i+1];
-                }
-                nottake = dp[i+1][j];
-
-                dp[i][j] = (take || nottake);
-            }
-        }
-        return dp[0][0];
+        vector<int>dp(n,-1);
+        return solve(0,s,set,dp);
     }
 };
