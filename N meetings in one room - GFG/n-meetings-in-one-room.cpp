928a77758cccea1c9876c29a60e1f1de411d05aa
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-08-09 13:15:56 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -10,28 +10,21 @@ class Solution
     //be performed in a meeting room.
     int maxMeetings(int start[], int end[], int n)
     {
-        vector<pair<int,int>>arr;
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++){
+            v.push_back({end[i],start[i]});
+        }
+        sort(v.begin(),v.end());
+        
+        int last = -1;
+        int count = 0;
         
         for(int i=0;i<n;i++){
-            arr.push_back({end[i],start[i]});
-        }
-        
-        sort(arr.begin(),arr.end());
-        
-        int last=-1,count=0;
-        
-        for(int i=0;i<n;i++){
-            if(last == -1){
+            if(last == -1 || last < v[i].second){
                 count++;
-                last = arr[i].first;
+                last = v[i].first;
             }
-            else if(last < arr[i].second){
-                count++;
-                last = arr[i].first;
-            }
-            else continue;
         }
-        
         return count;
     }
 };
