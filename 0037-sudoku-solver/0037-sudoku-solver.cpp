9a8518654f201e6ac48aa984cef0f1a1aa433b39
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-08-10 16:33:05 +0530

    Time: 25 ms (81.65%), Space: 6.3 MB (62.55%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    bool isSafe(char ch,int row,int col,vector<vector<char>> &board){
-        
+    bool issafe(int row,int col,char ch,vector<vector<char>>& board){
         for(int i=0;i<9;i++){
             if(board[i][col] == ch){
                 return false;
             }
         }
-        for(int i=0;i<9;i++){
-            if(board[row][i] == ch){
+        for(int j=0;j<9;j++){
+            if(board[row][j] == ch){
                 return false;
             }
         }
-        
-        row = row - row%3;
-        col = col - col%3;
+        row = row-row%3;
+        col = col-col%3;
         
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
@@ -23,30 +21,29 @@ public:
                 }
             }
         }
-        
         return true;
     }
     bool solve(vector<vector<char>>& board){
-        
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                
-                if(board[i][j]=='.'){
-                    
-                    for(char c='1';c<='9';c++){
-                        
-                        if(isSafe(c,i,j,board)){
-                            board[i][j]=c;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(issafe(i,j,ch,board)){
                             
-                            if(solve(board))
+                            board[i][j] = ch;
+                            
+                            if(solve(board)){
                                 return true;
-      
-                            else board[i][j]='.';
-
+                            }
+                            else{
+                                board[i][j] = '.';   
+                            }   
                         }
                     }
+                    
                     return false;
                 }
+                
             }
         }
         return true;
