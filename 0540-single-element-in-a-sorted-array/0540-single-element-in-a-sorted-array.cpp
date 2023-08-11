4cfc66944c270940eb65ebcb29e14ea2e4804dfa
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-08-11 15:10:59 +0530

    Time: 16 ms (95.61%), Space: 22.4 MB (30.21%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,19 +4,23 @@ public:
         int n = nums.size();
         int low = 0, high = n-1;
         
-        while(low<=high){
+        while(low <= high){
             int mid = (low+high)/2;
             
-            if(mid>0 && nums[mid] == nums[mid-1]){
-                if((mid-1-low)%2 == 0){
-                    low = mid+1;
+            if(mid > 0 && nums[mid] == nums[mid-1]){
+                int len = mid-low+1;
+                
+                if(len%2 != 0){
+                    high = mid-2;
                 }
                 else{
-                    high = mid-2;
+                    low = mid+1;
                 }
             }
             else if(mid < n-1 && nums[mid] == nums[mid+1]){
-                if((n-1-mid-1)%2 == 0){
+                int len = high-mid+1;
+                
+                if(len%2 == 0){
                     high = mid-1;
                 }
                 else{
