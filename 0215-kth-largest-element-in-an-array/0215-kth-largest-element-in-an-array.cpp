c08cba1a2b03ca0fe3b8b9671c992e81cd04407b
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-08-14 12:24:41 +0530

    Time: 188 ms (7.48%), Space: 45.4 MB (86.52%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-int partition(vector<int>& arr, int left, int right) {
-    int pivot = arr[left] ;
-    int l = left + 1 ;
-    int r = right;
-    while (l <= r) {
-        if (arr[l] < pivot && arr[r] > pivot) {
-            swap(arr[l], arr[r]);
-            l++ ;
-            r-- ;
+    int solve(int left,int right,vector<int>& nums){
+        int pivot = nums[left];
+        int l = left+1;
+        int r = right;
+        
+        while(l <= r){
+            if(nums[l] <= pivot && nums[r] >= pivot){
+                swap(nums[l],nums[r]);
+                l++;
+                r--;
+            }
+            else if(pivot <= nums[l]){
+                l++;
+            }
+            else if(pivot >= nums[r]){
+                r--;
+            }
         }
-        if (arr[l] >= pivot) {
-            l++;
-        }
-        if (arr[r] <= pivot) {
-            r--;
-        }
+        
+        swap(nums[left],nums[r]);
+        return r;
     }
-    swap(arr[left], arr[r]);
-    return r;
-}
-
     int findKthLargest(vector<int>& nums, int k) {
-        int left = 0, right = nums.size()-1;
+        int n = nums.size();
+        int left = 0, right = n-1;
         
         while(true){
-            int idx = partition(nums,left,right);
+        
+            int idx = solve(left,right,nums);
             
             if(idx == k-1){
                 return nums[idx];
             }
-            
-            if(idx >= k-1){
+            else if(idx > k-1){
                 right = idx-1;
             }
             else{
                 left = idx+1;
             }
         }
-        
         return -1;
     }
 };
