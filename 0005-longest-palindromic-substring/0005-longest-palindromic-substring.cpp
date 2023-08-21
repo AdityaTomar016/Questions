5f1ca20e6fd3afcd13f5a92a01324ba2eefb4834
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-08-22 02:15:54 +0530

    Time: 212 ms (42.74%), Space: 288.1 MB (11.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
-    
-    string longestPalindrome(string s) {
-        int i=0,j=0,n=s.size();
-        int maxi = 0;
-        string ans = "";
+    bool ispal(int i,int j,string &s,vector<vector<int>>&dp){
         
-       vector<vector<int>>dp(n,vector<int>(n,0));
+        if(i > j) return true;
         
-        for(int diff=0;diff<n;diff++){
-            int i=0,j=i+diff;
-            while(j<n){
-                
-                if(i == j){
-                    dp[i][j] = 1;
-                }
-                else if(diff == 1){
-                    if(s[i] == s[j]){
-                        dp[i][j] = 2;
-                    }
-                    else dp[i][j] = 0;
-                }
-                else{
-                    if(s[i] == s[j] && dp[i+1][j-1]){
-                        dp[i][j] = 2 + dp[i+1][j-1];
-                    }
-                }
-                
-                if(dp[i][j]){
-                    if(maxi < dp[i][j]){
-                        maxi = dp[i][j];
-                        ans = s.substr(i,j-i+1);
-                    }
-                }
-                
-                i++;
-                j++;
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        bool equal = false;
+        if(s[i] == s[j]){
+            if(j-i <= 2){
+                equal = true;
+            }
+            else{
+                equal = equal || ispal(i+1,j-1,s,dp);
             }
         }
         
-        return ans;
+        return dp[i][j] = equal;
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int maxi = 0;
+        string res = "";
+        
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                
+                if(s[i] == s[j] && ispal(i+1,j-1,s,dp)){
+                    if(j-i+1 > maxi){
+                        maxi = j-i+1;
+                        res = s.substr(i,j-i+1);
+                    }
+                }
+            }
+        }
+        
+        return res;
     }
 };
