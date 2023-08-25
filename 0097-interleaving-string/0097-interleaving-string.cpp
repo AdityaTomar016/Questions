a40fddb5e558956634e8760bd8e3b8ed4d1cb799
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-08-25 14:09:59 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (45.59%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -22,7 +22,36 @@ public:
         return dp[i][j] = first || second;
     }
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
-        return solve(0,0,0,s1,s2,s3,dp);
+        
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        if(s3.size()!=s1.size()+s2.size()){
+            return false;
+        }
+        int k=s3.size()-1;
+        
+        
+        for(int i=n;i>=0;i--){
+            for(int j=m;j>=0;j--){
+                
+                if(i == n && j == m){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int k = i+j;
+                bool first = false, second = false;
+        
+                if(s1[i] == s3[i+j]){
+                    first = dp[i+1][j];
+                }
+                if(s2[j] == s3[i+j]){
+                    second = dp[i][j+1];
+                }
+
+                dp[i][j] = first || second;
+            }
+        }
+        return dp[0][0];
     }
 };
