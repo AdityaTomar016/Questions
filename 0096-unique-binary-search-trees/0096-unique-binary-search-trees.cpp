295832da89a0c00d247d82014285309b3a62d394
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-08-30 16:01:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (13.73%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -17,7 +17,20 @@ public:
         return dp[n] = ans;
     }
     int numTrees(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        vector<int>dp(n+1,0);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            int ans = 0;
+        
+            for(int j=1;j<=i;j++){
+                ans += dp[j-1]*dp[i-j];
+            }
+
+            dp[i] = ans;
+        }
+        return dp[n];
     }
 };
