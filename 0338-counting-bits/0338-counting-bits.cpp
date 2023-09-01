2c1b1266d6e7a99f186e2ae834f7834b38dc2e8e
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-09-01 13:45:38 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (32.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    int count_bits(int n){
+        int count=0;
+        while(n > 0){
+            count += n&1;
+            n = n>>1;
+        }
+        return count;
+    }
     vector<int> countBits(int n) {
         vector<int>res;
         for(int i=0;i<=n;i++){
