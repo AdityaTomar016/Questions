074f94730e74523b893ac61b8ab102bc1f352f49
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-10-27 11:26:17 +0530

    Time: 3 ms (70.37%), Space: 9.9 MB (34.84%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,15 +4,18 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         
-        int low = 0,high = m-1;
+        int i=0,j=m-1;
         
-        while(low < n && high >= 0){
-            int curr = matrix[low][high];
-            
-            if(curr == target) return true;
-            
-            if(target > curr) low++;
-            else high--;
+        while(i<n && j>=0){
+            if(target == matrix[i][j]){
+                return true;
+            }
+            else if(target > matrix[i][j]){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
         
         return false;
