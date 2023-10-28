3e339f3ac8c480fe53514d6f6ca5b063356904bc
Aditya Tomar <97149793+AdityaTomar016@users.noreply.github.com>
2023-10-28 21:34:04 +0530

    Time: 48 ms (69.81%), Space: 43.8 MB (48.47%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne040868676844736584divamazon-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne015278231746483795divgoogle-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne0662947165877652divtwitter-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne010745317729027026divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne010690041324665356divsnapchat-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne041622643653328417divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne04115516061634228divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne09854440687061932divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne09596260044331453divbytedance-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fillhsla216-60-60-1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div208-implement-trie-prefix-tree/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne040868676844736584divamazon-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne015278231746483795divgoogle-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne0662947165877652divtwitter-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne010745317729027026divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne010690041324665356divsnapchat-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne041622643653328417divapple-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne04115516061634228divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne09854440687061932divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba6-12-24-005-ne09596260044331453divbytedance-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fillhsla216-60-60-1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,75 @@
+struct Node{
+    Node *links[26];
+    bool flag=false;
+    
+    bool containskey(char ch){
+        return links[ch-'a'] != NULL;
+    }
+    
+    void put(char ch){
+        links[ch-'a'] = new Node();
+    }
+    
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    
+};
+class Trie {
+public:
+    Node *root;
+    
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        
+        Node *node = root;
+        
+        for(int i=0;i<word.size();i++){
+            if(!node->containskey(word[i])){
+                node->put(word[i]);
+            }
+            node = node->get(word[i]);
+        }
+        
+        node->flag = true;
+    }
+    
+    bool search(string word) {
+        
+        Node* node = root;
+        
+        for(int i=0;i<word.size();i++){
+            if(!node->containskey(word[i])){
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        
+        return node->flag;
+    }
+    
+    bool startsWith(string prefix) {
+        
+        Node* node = root;
+        
+        for(int i=0;i<prefix.size();i++){
+            if(!node->containskey(prefix[i])){
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
